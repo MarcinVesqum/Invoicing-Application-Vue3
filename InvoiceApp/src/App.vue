e247f298478e7f7ae4664064f293debffa643a9c
Vesqum <marcin.pelszyk30@gmail.com>
2022-09-09 09:25:23 +0200

    added sidebar

--- InvoiceApp/src/App.vue
+++ InvoiceApp/src/App.vue
@@ -1,13 +1,145 @@
 <template>
-  <div class="app">
-    
+  <div>
+    <div class="app flex flex-colum">
+      <Sidebar />
+    <div class="app-content flex flex-column">
+        <router-view />
+    </div>
   </div>
+  </div>
+  
 </template>
 
 <script setup>
 
+import Sidebar from './components/Sidebar.vue';
+import { useStore } from 'vuex';
+import { computed } from '@vue/runtime-core';
+
+const store = useStore();
+
+const invoicesLoaded = computed(() => store.store.invoicesLoaded);
+store.dispatch('GET_INVOICES')
+
+
+
+
 </script>
 
-<style scoped>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  font-family:"Poppins" ,sans-serif;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+.app {
+  background-color: #355764;
+  min-height: 100vh;
+
+  .app-content {
+  padding: 0 20px;
+  flex: 1;
+  position: relative;
+  }
+}
+
+
+
+button,
+.button {
+  cursor: pointer;
+  padding: 1rem 2.6rem;
+  border-radius: 3rem;
+  border: none;
+  font-size: 1.4rem;
+  margin-right: 0.8rem;
+  color: #ffff;
+
+}
+
+.dark-green-alt {
+  background-color: #355764;
+}
+.dark-green {
+  background-color: #30483f;
+}
+.red {
+  background-color: #ec5757;
+}
+.blue {
+  background-color: #81CACF;
+}
+.green {
+  background-color: #33d69f;
+}
+.yellow {
+  background-color: #FFEA11;
+}
+
+// Utility class
+.flex {
+  display: flex;
+}
+.flex-column {
+  flex-direction: column;
+}
+
+.container {
+  width: 100%;
+  padding: 4rem 1rem;
+  max-width: 850px;
+  margin: 0 auto;
+  @media (min-width: 900px) {
+    padding-top: 7.2rem;
+  }
+}
+.status-button {
+  &::before {
+    content: '';
+    width: 1rem;
+    height: 1rem;
+    border-radius: 50%;
+    margin-right: 0.8rem;
+  }
+  font-size: 1.2rem;
+  margin-right: 3rem;
+  align-items: center;
+  padding: 0.8rem 3rem;
+  border-radius: 1rem;
+}
+
+.paid {
+  &::before {
+    background-color: #33d69f;
+  }
+  color: #33d69f;
+  background-color: rgba(51, 214, 160, 0.1);
+}
+
+.pending {
+  &::before {
+    background-color: #FFEA11;
+  }
+  color: #FFEA11;
+  background-color: rgba(142, 136, 9, 0.1);
+}
+
+.draft {
+  &::before {
+    background-color: #81CACF;
+  }
+  color: #81CACF;
+  background-color: rgba(13, 150, 139, 0.1);
+}
+
 
 </style>
---
+++ InvoiceApp/src/components/Sidebar.vue
@@ -0,0 +1,201 @@
+<template>
+    <aside class="flex flex-column" :class="`${activeMenu && 'active-menu'}`">
+        <div class="logo flex">
+            <img src="../assets/file-invoice-dollar-solid.png" alt="">
+        </div>
+        <div class="menu-toggle-wrapper flex" @click="toggleIcon">
+            <div class="menu-toggle">
+                <span class="material-icons">
+                    keyboard_double_arrow_right
+                </span>
+            </div>
+        </div>
+        <h3>Menu</h3>
+        <div class="menu">
+            <router-link class="button-sidebar" :to="{ name: 'Home'}">
+                <span class="material-icons">
+                    home
+                </span>
+                <span class="text">Home</span>
+            </router-link>
+            <router-link  class="button-sidebar" :to="{ name: 'NewInvoice'}">
+                <span class="material-icons">
+                    screenshot_monitor
+                </span>
+                <span class="text">Nowa Faktura</span>
+            </router-link>
+
+        </div>
+
+        <div class="flex-1">
+        </div>
+
+        <div class="menu">
+            <router-link  class="button-sidebar" :to="{ name: 'Login'}">
+                <span class="material-icons">
+                    settings
+                </span>
+                <span class="text">Logowanie</span>
+            </router-link>
+            <li  class="logout" @click="logout">Wyloguj</li>
+        </div>
+
+    </aside>
+</template>
+
+<script setup>
+import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
+import { computed, ref } from 'vue';
+
+const store = useStore();
+const router = useRouter();
+const activeMenu = ref(localStorage.getItem('activeMenu') === 'true')
+
+
+// const user = computed(() => store.state.users.user)
+
+const toggleIcon = () => {
+    activeMenu.value = !activeMenu.value
+    localStorage.setItem('activeMenu', activeMenu.value)
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+aside {
+
+    width: calc(2rem + 32px);
+    padding: 1rem;
+    overflow: hidden;
+    min-height: 100vh;
+    background-color: #5A8F7B;
+    position: fixed;
+    color: #ffff;
+    z-index: 1001;
+    
+        .logo {
+        position: relative;
+        border-radius: 0 20px 20px 0;
+        margin-bottom: 1rem;
+        top: -1rem;
+        left: -1rem;
+        height: 80px;
+        width: 50px;
+        background-color: #81CACF;
+        padding: 2rem;
+        justify-content: center;
+
+        
+            img {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                width: 2rem;
+            }
+        }
+        
+        h3 {
+            margin-bottom: 1rem;
+            font-size: 1.5rem;
+            color: #fff;
+        }
+        .menu-toggle-wrapper {
+            justify-content: flex-end;
+            margin-bottom: 1rem;
+            position: relative;
+            top: 0;
+            left: 0;
+            transition:  .2s ease-in;
+            .menu-toggle {
+                transition:  .4s ease-out;
+                .material-icons {
+                    cursor: pointer;
+                    font-size: 3rem;
+                    color: #fff;
+                }
+                &:hover {
+                    .material-icons {
+                        color: #81CACF;
+                        
+                        transform: translateX(0.5rem);
+                        transition: .4s ease-out;
+                    }
+                }
+            }
+        }
+        .flex-1 {
+            flex: 1 1 0;
+        }
+        h3, .button-sidebar .text {
+            opacity: 0;
+            transition: .3s ease-in;
+        }
+        .menu {
+            // margin: 0 -1rem;
+            display: flex;
+            flex-direction: column;
+            gap: 2rem 0;
+            
+            .button-sidebar {
+                display: flex;
+                align-items: center;
+                text-decoration: none;
+                margin-left: -0.7rem;
+                transition: all 0.2s ease-in-out;
+                // padding: 0.5rem 1rem;
+                .material-icons {
+                    font-size: 3rem;
+                    color: #fff;
+                    transition: all 0.5s ease-in-out;
+                }
+                .text {
+                    color: #fff;
+                    margin-left: 1rem;
+                    transition: all  0.5s ease-in-out;
+                }
+                &:hover {
+                    background-color: #459f7e;
+                    .material-icons, .text {
+                        color: #fff;
+                    }
+                    .text {
+                        transform: scale(1.1);
+                    }
+                }
+                &.router-link-exact-active {
+                    margin-right: -0.7rem;
+                    border-right: 5px solid #459f7e;
+                    .material-icons, .text {
+					    color: #fff;
+				    }
+
+
+                }
+            }
+        }
+        &.active-menu {
+            width: 200px;
+
+            .menu-toggle-wrapper {
+                top: -3rem;
+
+                .menu-toggle {
+                    transform: rotate(-180deg);
+                }
+
+            }
+            h3, .button-sidebar .text {
+                opacity: 1;
+                transition: .3s ease-in;
+                }
+        }
+
+
+
+        
+
+}
+</style>
